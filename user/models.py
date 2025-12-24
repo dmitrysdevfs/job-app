0c66bca68b6958e665b0ba93e12a64d935d903c9
Dmytro Salyakin <d.salyakin@gmail.com>
2025-12-25 01:22:06 +0200

    Extend User model with roles (Candidate/Recruiter)

--- user/models.py
+++ user/models.py
@@ -1,6 +1,28 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    class Type(models.TextChoices):
+        CANDIDATE = 'CANDIDATE', 'Шукач'
+        RECRUITER = 'RECRUITER', 'Рекрутер'
+
+    user_type = models.CharField(
+        max_length=10,
+        choices=Type.choices,
+        default=Type.CANDIDATE,
+        verbose_name="Тип користувача"
+    )
+
+    @property
+    def is_candidate(self):
+        return self.user_type == self.Type.CANDIDATE
+
+    @property
+    def is_recruiter(self):
+        return self.user_type == self.Type.RECRUITER
+
+    class Meta:
+        verbose_name = "Користувач"
+        verbose_name_plural = "Користувачі"
 
 

49e32c5f21e24aa2c0ba24c54600d30a903b0150
Dmytro Salyakin <d.salyakin@gmail.com>
2025-12-24 02:39:13 +0200

    added: Employer detail view with paginated vacancies and UI improvements

--- employer/urls.py
+++ employer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EmployerListView
+from .views import EmployerListView, EmployerDetailView
 
 app_name = "employer"
 
 urlpatterns = [
     path("", EmployerListView.as_view(), name="list"),
+    path("<int:pk>/", EmployerDetailView.as_view(), name="detail"),
 ]
--- employer/views.py
+++ employer/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Employer
+from core.utils import get_paginated_page
 
 class EmployerListView(ListView):
     """Список роботодавців з використанням Class-Based Views"""
@@ -9,4 +10,21 @@ class EmployerListView(ListView):
     paginate_by = 9
     queryset = Employer.objects.all().order_by('name')
 
-# Create your views here.
+class EmployerDetailView(DetailView):
+    """Детальна сторінка роботодавця зі списком його вакансій"""
+    model = Employer
+    template_name = "employer/employer_detail.html"
+    context_object_name = "employer"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Підтягуємо вакансії цього роботодавця
+        vacancies_qs = self.object.vacancies.all().order_by('-published_at')
+        
+        # Використовуємо універсальну пагінацію для списку вакансій (7 на сторінку)
+        page_obj = get_paginated_page(self.request, vacancies_qs, per_page=7)
+        
+        context['vacancies'] = page_obj.object_list
+        context['page_obj'] = page_obj
+        context['total_vacs'] = vacancies_qs.count()
+        return context
